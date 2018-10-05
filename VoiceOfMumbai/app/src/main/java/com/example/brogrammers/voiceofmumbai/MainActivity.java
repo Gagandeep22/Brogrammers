8184ac2bb3d59afc7bf3c9ade64be560b0d51197
Rushabh <36025880+rushabhD97@users.noreply.github.com>
2018-10-05 19:06:36 +0530

    runningActivity

--- VoiceOfMumbai/app/src/main/java/com/example/brogrammers/voiceofmumbai/MainActivity.java
+++ VoiceOfMumbai/app/src/main/java/com/example/brogrammers/voiceofmumbai/MainActivity.java
@@ -1,12 +1,58 @@
 package com.example.brogrammers.voiceofmumbai;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import android.support.design.widget.BottomNavigationView;
+import android.view.MenuItem;
+
 public class MainActivity extends AppCompatActivity {
+    private FirebaseAuth mAuth;
+    private FirebaseUser mUser;
+    private ActionBar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mAuth=FirebaseAuth.getInstance();
+        mUser=mAuth.getCurrentUser();
+        if(mUser==null) {
+            startActivity(new Intent(MainActivity.this, RegisterActivity.class).addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP).addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK));
+            finish();
+            return;
+        }
+        toolbar=getSupportActionBar();
+        BottomNavigationView navigation = (BottomNavigationView ) findViewById(R.id.navigationView);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        toolbar.setTitle("Recents");
     }
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            Fragment fragment;
+            switch (item.getItemId()) {
+                case R.id.navigation_recents:
+                    toolbar.setTitle("Recents");
+                    return true;
+
+                case R.id.navigation_profile:
+                    toolbar.setTitle("Profile");
+                    return true;
+            }
+            return false;
+        }
+    };
+
 }
